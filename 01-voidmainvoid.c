62fc6d1449895eac8916c74be32db49bfb2a351f
omkarphale <omkarphale9@gmail.com>
2024-06-07 15:47:15 +0530

    added : simple function prototype for the entry-point function - void main(void).

---
+++ 01-voidmainvoid.c
@@ -0,0 +1,15 @@
+/*******************************************************************
+ * File Name         : 01-voidmainvoid.c
+ * Description       : In this file, you'll find the simple function prototype for the entry-point function.
+ * Revision History  :
+ * ------------------------------------------------------------------
+ * Date		    | Author
+ * ------------------------------------------------------------------
+ * 07/11/2014	| Omkar Phale
+ ********************************************************************/
+
+// entry-point function
+void main(void)
+{
+    // TODO:
+}
