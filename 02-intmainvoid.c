fa53893e5127b2651ec20d68ca34381f79304484
omkarphale <omkarphale9@gmail.com>
2024-06-07 15:47:41 +0530

    added : function prototype for the entry-point function returning an integer value - int main(void).

---
+++ 02-intmainvoid.c
@@ -0,0 +1,16 @@
+/*******************************************************************
+ * File Name         : 01-voidmainvoid.c
+ * Description       : In this file, you'll find the function prototype for the entry-point function returning an integer value.
+ * Revision History  :
+ * ------------------------------------------------------------------
+ * Date		    | Author
+ * ------------------------------------------------------------------
+ * 07/11/2014	| Omkar Phale
+ ********************************************************************/
+
+// entry-point function
+int main(void)
+{
+    // TODO:
+    return (0);
+}
