542b481a7f40865cac08c7cd208e986a6fd8ef84
omkarphale <omkarphale9@gmail.com>
2024-06-07 16:02:53 +0530

    added : function prototype for the entry-point function that returns an integer value and takes command-line arguments as `argc` (argument count).

---
+++ 04-intmainintargc.c
@@ -0,0 +1,22 @@
+/*******************************************************************
+ * File Name         : 01-voidmainvoid.c
+ * Description       : In this file, you'll find the function prototype for the entry-point function that returns an integer value and takes command-line arguments as `argc` (argument count).
+ * Revision History  :
+ * ------------------------------------------------------------------
+ * Date		    | Author
+ * ------------------------------------------------------------------
+ * 07/06/2024	| Omkar Phale
+ ********************************************************************/
+
+/*
+ * Function:  main()
+ * --------------------
+ * argc : this is the first commandline argument which contains the total number of commandline arguements passed.
+ * returns : zero.
+ */
+
+void main(int argc)
+{
+    // TODO:
+    return (0);
+}
